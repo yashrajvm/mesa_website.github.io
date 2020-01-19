0625ffd9fa06e1ae2c4efe6851b523413df96e11
prajwlr <prajwalraut2000@gmail.com>
2020-01-20 00:08:44 +0530

    Background color changes on blog site

---
+++ Mechanchies' Window/javascript/index.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+	function bindNavbar() {
+		if ($(window).width() > 768) {
+			$('.navbar-inverse .dropdown').on('mouseover', function(){
+				$('.dropdown-toggle', this).next('.dropdown-menu').show();
+			}).on('mouseout', function(){
+				$('.dropdown-toggle', this).next('.dropdown-menu').hide();
+			});
+			
+			$('.dropdown-toggle').click(function() {
+				if ($(this).next('.dropdown-menu').is(':visible')) {
+					window.location = $(this).attr('href');
+				}
+			});
+		}
+		else {
+			$('.navbar-inverse .dropdown').off('mouseover').off('mouseout');
+		}
+	}
+	
+	$(window).resize(function() {
+		bindNavbar();
+	});
+	
+	bindNavbar();
+});
+
+// $(window).on('load',function() { //when the document is loaded
+//   $(".loader-wrap").delay(50000).fadeOut("slow"); //fade out preloader. You can add your custom effects here too.
+// });
+
+// function loaderDestroy(){
+// 	document.getElementById("loader").remove();
+// }
+
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+  
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
