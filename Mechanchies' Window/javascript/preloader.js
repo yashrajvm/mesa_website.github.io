2a90a685d7582c580971d20ab8a9377d86802875
prajwlr <prajwalraut2000@gmail.com>
2020-01-20 16:17:43 +0530

    preloader added

---
+++ Mechanchies' Window/javascript/preloader.js
@@ -0,0 +1,3 @@
+$(window).on('load',function() { //when the document is loaded
+  $(".loaderback").delay(3000).fadeOut("slow"); //fade out preloader. You can add your custom effects here too.
+});
--- Mechanchies' Window/javascript/temp.js
+++ Mechanchies' Window/javascript/temp.js
@@ -1,29 +1,3 @@
-$(window).scroll(function() {
-  
-  // selectors
-  var $window = $(window),
-      $body = $('body'),
-      $panel = $('.panel');
-  
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
- 
-  $panel.each(function () {
-    var $this = $(this);
-    
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-          
-      // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
-       
-      // Add class of currently active div
-      $body.addClass('color-' + $(this).data('color'));
-    }
-  });    
-  
-}).scroll();
+$(window).on('load',function() { //when the document is loaded
+  $(".loading").delay(1000).fadeOut("slow"); //fade out preloader. You can add your custom effects here too.
+});
